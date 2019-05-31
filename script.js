9e679d1059a96acfeacabfca6c939177c43c0962
Teng Lin <tenglin2@gmail.com>
2019-05-31 19:51:18 -0400

    Removed stacks

--- script.js
+++ script.js
@@ -7,38 +7,7 @@
 
 // START
 
-// Stacks
-const Stack = function() {
-  this.count = 0;
-  // We implement it as an object because an array is already a stack implementation.
-  this.storage = {};
-  
-  this.push = function(element) {
-    this.storage[this.count] = element;
-    count += 1;
-  };
 
-  this.pop = function() {
-    // In case pop is run on empty stack...
-    if (this.count === 0) {
-      return undefined;
-    }
-
-    let result = this.storage[this.count];
-    delete this.storage[this.count - 1]; // Removes the last element.
-    count -= 1;
-
-    return result;
-  };
-
-  this.size = function() {
-    return this.count;
-  };
-
-  this.peek = function() {
-    return this.storage[this.count - 1];
-  };
-};
 
 // Sets --> Can only have distinct elements, no repeats.
 const mySet = function() {
