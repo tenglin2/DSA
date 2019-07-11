20568b5ec5028e32d672357856cded46cd0beb71
Teng Lin <tenglin2@gmail.com>
2019-07-11 18:09:39 -0400

    selection sort clean code

--- algorithms/SelectionSort.js
+++ algorithms/SelectionSort.js
@@ -1,29 +1,28 @@
 /**
- * Selection sort works somewhat similar to bubble sort except we focus on having the minimum values in the front instead of maximum values in the back. Likewise, we have a pivot that continues to progress through each pass so that we don't have to do a complete O(n^2) time complexity.
- * The trick to remembering this is think about the pivot and how it will progress from index 0 to index arr.length - 2 because the last swap will be not the last element but the second last element. The condition will be < arr.length - 1 which is the same meaning.
- * Another thing to keep in mind is that we don't do a swap operation until we complete a full pass. This is unlike bubble sort where we swap at every instance of true conditionals.
+ * Selection Sort focuses on having the minimum values in the front instead of maximum values in the back in Bubble Sort.
+ * Keep in mind is that we don't do a swap operation until we complete a full pass. This is unlike bubble sort where we swap at every instance of true conditionals.
  * Compare with the minIndex and not i. Remember it can overwrite the current smallest.
+ * The time complexity is O(n^2) and it is probably the worst of the elementary sorting algorithms. It has no special use cases like almost sorted Bubble Sort or live sorting Insertion Sort. Nevertheless, it is probably the easiest to understand.
+ * REMINDERS: Finding smallest number with each pass and swapping only once after each pass. Do not handle the last index because it is already sorted.
  */
 const selectionSort = function(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
-    }
+	// Progress until second last element, not last element because by end, last element will already be in correct position.
+	for (let i = 0; i < arr.length - 1; i++) {
+		// minIndex is taken for the eventual swap with each pass.
+		let minIndex = i;
 
-    console.log('before swap', arr);
-    
-    // One pass has completed...
-    if (minIndex !== i) {
-      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-    }
-    
-    console.log('after swap', arr);
-  }
+		// Loop through the rest of the array and find the minimum value's index.
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[minIndex]) {
+				minIndex = j;
+			}
+		}
 
-  return arr;
+		// Swap step.
+		if (minIndex !== i) {
+			[ arr[i], arr[minIndex] ] = [ arr[minIndex], arr[i] ];
+		}
+	}
+
+	return arr;
 };
-
-console.log(selectionSort([93,8,12, 26, 100, 99]));
