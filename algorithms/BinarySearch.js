87efe079ae2b0c5e4216aa0ca8c59e3e8d1eb9cb
Teng Lin <tenglin2@gmail.com>
2019-07-11 17:55:29 -0400

    binary search clean code

--- algorithms/BinarySearch.js
+++ algorithms/BinarySearch.js
@@ -1,45 +1,25 @@
 /**
- * Important to keep in mind that binary search only works with sorted iterative data. There are definitely some complications with flooring and rounding.
- * From memory it is O(logn) because you are splitting the array in approximately by half with each step.
+ * Important to keep in mind that binary search only works with sorted iterative data. The complexity for search is O(logn) which is exceptional, but only applicable if the data is sorted.
  */
-
-function binarySearch(array, val) {
-  let min = 0;
-  let max = array.length - 1;
-
-  // We use an equality operator because we don't want to exclude the case where it's only one element left.
-  while (min <= max) {
-    // Finding the middle index and using a 
-    let middle = Math.floor((min + max) / 2);
-    let currentElement = array[middle];
-
-    if (array[middle] < val) {
-      min = middle + 1;
-    } else if (array[middle] > val) {
-      max = middle - 1;
-    } else return middle;
-  }
-
-  return -1;
-}
-
-
-// Redone - Note that you have to floor the middle index because javascript doesn't automatically do this. It is not strong typed for ints and doubles.
 const binarySearch = function(arr, target) {
-  let start = 0;
-  let end = arr.length - 1;
+	let start = 0;
+	let end = arr.length - 1;
 
-  while (start <= end) {
-    let middle = Math.floor((start + end) / 2);
+	// We use the condition start <= end including when start === end, because we need to check that case inside of the loop. This is done in the step when (arr[middle] === target). It is necessary that we check this instance.
+	while (start <= end) {
+		// We use a flooring function which favors the lower half when dealing with even number of elements. This is preference, Math.ceil() will favor the upper half for middle index.
+		let middle = Math.floor((start + end) / 2);
 
-    if(arr[middle] === target) {
-      return middle;
-    } else if (arr[middle] > target) {
-      end = middle - 1;
-    } else if (arr[middle] < target) {
-      start = middle + 1;
-    }
-  }
+		if (arr[middle] === target) {
+			return middle;
+		} else if (arr[middle] > target) {
+			end = middle - 1;
+		} else if (arr[middle] < target) {
+			start = middle + 1;
+		} else return null;
+		// Another else case should never happen. Null just for stylistic reasons.
+	}
 
-  return -1;
+	// If the target is not found in the array we return an index of -1, DNE.
+	return -1;
 };
